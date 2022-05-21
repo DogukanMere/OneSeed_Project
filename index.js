a38957ea263918c20b8a1813189683b7ed0eb03e
Dan Mere <meredogukan@gmail.com>
2022-05-20 23:54:19 -0400

    OneSeed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8888; //set up port 8888
 
+// Pug
 app.set("views", path.join(__dirname, "views")); // set <app_directory>/views
 app.set("view engine", "pug");
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+//DOM//
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
@@ -7,18 +8,17 @@ const contactSubmit = document.querySelector(".contact__submit");
 const divForm = document.querySelector(".confirm__flex");
 const confirmMsg = document.querySelector(".confirm__message");
 
-console.log(hamburger);
-
+// Hamburger Menu Items
 menuItems.forEach((menuItem) => {
   menuItem.addEventListener("click", toggleMenu);
 });
 
+// Hamburger Menu Icon
 hamburger.addEventListener("click", toggleMenu);
 
+// Function
 function toggleMenu() {
-  console.log("dsds");
   if (menu.classList.contains("showMenu")) {
-    console.log("clicked");
     menu.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
@@ -29,6 +29,7 @@ function toggleMenu() {
   }
 }
 
+// Form Send Function
 contactSubmit.addEventListener("click", (e) => {
   e.preventDefault();
   divForm.classList.add("contact__animation");
