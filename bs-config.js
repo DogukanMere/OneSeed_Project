5bc1922377cd78dc572dd58dc2b29bf1a7b42c07
Dogukan (Dan) Mere <meredogukan@gmail.com>
2023-01-19 09:49:58 -0500

    port changed

--- bs-config.js
+++ bs-config.js
@@ -13,10 +13,10 @@
  */
 module.exports = {
   ui: false,
-  files: ["**/*.css", "**/*.pug", "**/*.js"],
-  ignore: ["node_modules"],
-  proxy: "localhost:8888",
-  port: 3000,
+  files: ['**/*.css', '**/*.pug', '**/*.js'],
+  ignore: ['node_modules'],
+  proxy: 'localhost:8888',
+  port: 5000,
   reloadDelay: 10,
   notify: false,
 };
